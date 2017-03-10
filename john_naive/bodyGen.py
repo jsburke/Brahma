3ae51485ddf5e027f609be37af820ba5486ca244
jsburke <jsburke@bu.edu>
2017-03-10 16:34:38 -0500

    started filling out main, some constants added

--- john_naive/bodyGen.py
+++ john_naive/bodyGen.py
@@ -27,7 +27,42 @@
 # Rev.    : 0.1
 #
 
-import os, sys, subprocess, argparse, random
+import os, sys, argparse, random
+
+#-------------------------------------------------------
+# Generic space numbers
+#-------------------------------------------------------
+
+SOLAR_MASS = 1.99e30 # in kg
+EARTH_MASS = 5.9722e24
+JUPITER_MASS = 317.8 * EARTH_MASS
+COMET_SMALL_MASS = 1e13
+CERES_MASS = 8.958e20
+
+#--------------------------------------------------------
+# Relative Body masses and velocities
+#
+# Included to make sure we don't have something too wacky
+#--------------------------------------------------------
+
+bhole_mass_min = 5 * SOLAR_MASS
+bhole_mass_max = 200 * SOLAR_MASS
+
+star_mass_min = 0.08 * SOLAR_MASS
+star_mass_max = 12 * SOLAR_MASS
+
+planet_mass_min = .025 * EARTH_MASS # little less than 1/2 Mercury
+planet_mass_max = 17 * EARTH_MASS	# Kepler-10c
+
+giant_mass_min = 0.22 * JUPITER_MASS	# Bit smaller than Saturn 
+giant_mass_max = 12 * JUPITER_MASS	# just below low mass dwarf 13
+
+moon_mass_min = 0.008 * EARTH_MASS	# Europa
+moon_mass_max = 0.026 * EARTH_MASS  # a midge bigger than Ganymede
+
+minor_mass_min = COMET_SMALL_MASS
+minor_mass_max = CERES_MASS
+
 
 #--------------------------------------------------------
 # Command Line
@@ -88,6 +123,66 @@ def parse_cmd():
 def main():
 	options = parse_cmd()
 
-	print("\nhello\n")
+	# set up body parameters from command line
+
+	total 		= 0;	# zero will be used to show we don't care
+	remainder	= 0;	# total - all others
+	bhole 	  	= 0;
+	star 		= 0;
+	planet 		= 0;
+	giant 		= 0;
+	moon 		= 0;
+	minor 		= 0;
+
+	verbose_on	= 0;	# assume user doesn't want verbose output
+	if(options.verbose):
+		print("Verbose output enabled\n")
+		verbose_on = 1
+
+	# get number of each type
+
+	if (options.total):
+		total = options.total
+	if (options.bhole):
+		bhole = options.bhole
+	if (options.star):
+		star = options.star
+	if (options.planet):
+		planet = options.planet
+	if (options.giant):
+		giant = options.giant
+	if (options.moon):
+		moon = options.moon
+	if (options.minor):
+		minor = options.minor
+
+	# calculate total and unspecified remainder		
+
+	bodySum = bhole + star + planet + giant + moon + minor
+	if(verbose_on): print("Total number of bodies specified: %d" % bodySum)
+
+	if(total < bodySum):
+		total = bodySum
+		print(" ")
+	else: 
+		remainder = total - bodySum
+		print("Number of bodies unspecified: %d\n" % remainder)
+
+	# produce ouput file
+
+	filename = "galaxy_" + str(total) + ".csv"
+	if(verbose_on): print("Output file: %r" % filename)
+
+	outfile = open(filename, 'w')
+	outfile.truncate()
+
+	# generate the bodies and write to file
+
+	for i in xrange(0, bhole):
+		print(i)
+
+	outfile.close()
+	if(verbose_on): print("Complete Success!  Shoot for the stars!")
+
 
 main()	
