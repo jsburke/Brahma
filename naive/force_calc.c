c923af2e21ee153e918f1da4cffe0f85052cb570
sarthakjagetia <sarthakj@bu.edu>
2017-03-19 22:21:28 -0400

    Progress so far..

--- naive/force_calc.c
+++ naive/force_calc.c
@@ -1,10 +1,11 @@
 /*
+gcc force_calc.c
+./a.out galaxy_267.csv
 File: force_calc
 To calculate force on ever heavenly body by every other body
 Author: Sarthak Jagetia
 Date created: 03/18/2017
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,29 +25,27 @@ struct nbody{
 };
 struct nbody nbodies[MAX_BODIES];
 
-int force_calc(&filename){
-	csvfile = *filename;
-	FILE *bodyGenFile;
 
-    /* allocation of the buffer for every line in the File */
+
+int force_calc(FILE *s){
     char *buf = malloc(MAX_STR_LEN);
     char *tmp; 
 
-    /* if the space could not be allocated, return an error */
+    // if the space could not be allocated, return an error 
     if (buf == NULL) {
         printf ("Memory Allocation Error\n");
         return 1;
     }
    
        
-    if ( ( bodyGenFile = fopen( csvfile, "r" ) ) == NULL ) //Reading a file
+    if ( s == NULL ) //Reading a file
     {
         printf( "File could not be opened.\n" );
     }
 
 
     int i = 0;
-    while (fgets(buf, 255, bodyGenFile) != NULL)
+    while (fgets(buf, 255, s) != NULL)
     {
         if ((strlen(buf)>0) && (buf[strlen (buf) - 1] == '\n'))
             buf[strlen (buf) - 1] = '\0';       
@@ -79,14 +78,17 @@ int force_calc(&filename){
 
         i++;
     }
+
+    
     free(buf);
-    fclose(bodyGenFile);
+    fclose(s);
+    return 1;
 
-}
+}//*/
 
+//////////////////////////////////////////////////////////////main/////////////////////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char **argv)
-{   
+int main(int argc, char **argv){   
     /* To Extract Number of Bodies from file name */
     int total_n = 0;
     int n;
@@ -95,12 +97,20 @@ int main(int argc, char **argv)
     {
     	total_n += n;
         //printf("%d\n", total_n);
-    	printf("%d\n", j);
+    	//printf("%d\n", j); //prints the number in the file name eg: prints 10 of galaxy_10.csv
     }
-    int *filename;
-    *filename = argv[1]; //storing the filename 
+    FILE *filename;
+    filename = fopen(argv[1],"r");
+
+    //char a = argv[1];
+    //filename = &a;
     force_calc(filename);
+    //FILE a = argv[1]; //storing the filename 
+    //filename = &a;
+    //printf("%s",a);
+    //printf("\n");
+   // force_calc(filename);
 
-
+    fclose(filename);
     return 0;
 }
