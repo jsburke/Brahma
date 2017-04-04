b0883097e3cd7545edb32d935249d3c99012f802
jsburke <jsburke@bu.edu>
2017-04-04 14:02:44 -0400

    fix text alignment

--- octree/octree.h
+++ octree/octree.h
@@ -5,6 +5,8 @@ typedef data_t   float;
 
 typedef enum octant_type {ROOT, LVL_1, LVL_2} octant_type;  //  LVL_2 has no octant children, but uses body arrays
 
+//  body struct for reading from file
+
 typedef struct nbody{
     //char *category; 
 	data_t	 mass;
@@ -15,6 +17,10 @@ typedef struct nbody{
 	data_t	 vel_y;
 	data_t	 vel_z;
 } nbody;
+
+void		nbody_enum(nbody **body_array, char* file);
+
+//  octant struct and functions
 
 typedef struct octant
 {
@@ -45,15 +51,12 @@ typedef struct octant
 
 } octant, *p_octant;  //  octant is a cube divided into 8 cubes inside it of equal size
 
-p_octant octant_new(int type, int oct_no);
+p_octant 	octant_new(int type, int oct_no);
 
-void 	octant_center_of_mass(p_octant oct);
-void	octant_add_child(p_octant oct, p_octant child);
+void 		octant_center_of_mass(p_octant oct);
+void		octant_add_child(p_octant oct, p_octant child);
 
-void 	octant_add_body(p_octant oct, nbody* body);
-void 	octant_del_body(p_octant oct, nbody* body);
-
-
-
+void 		octant_add_body(p_octant oct, nbody* body);
+void 		octant_del_body(p_octant oct, nbody* body);
 
 #endif
