b78aeff1de8c2d4d63e8a007a4681ab8b583190a
jsburke <jsburke@bu.edu>
2017-04-04 13:27:02 -0400

    add prototypes

--- octree/octree.h
+++ octree/octree.h
@@ -3,9 +3,22 @@
 
 typedef data_t   float;
 
+typedef enum octant_type {ROOT, LVL_1, LVL_2} octant_type;  //  LVL_2 has no octant children, but uses body arrays
+
+typedef struct nbody{
+    //char *category; 
+	data_t	 mass;
+	data_t	 pos_x;
+	data_t	 pos_y;
+	data_t	 pos_z;
+	data_t	 vel_x;
+	data_t	 vel_y;
+	data_t	 vel_z;
+} nbody;
+
 typedef struct octant
 {
-	int 	octant_no;
+	int 	octant_no;  // -1 if it is root node
 
 	// center of mass for the octant
 	data_t 	mass_center_x;
@@ -18,6 +31,8 @@ typedef struct octant
 
 	// leaf arrays
 	// malloc to create array
+	// NULL if no leafs
+	int 	leaf_count;
 	data_t 	*mass;
 
 	data_t 	*pos_x;
@@ -29,5 +44,16 @@ typedef struct octant
 	data_t 	*vel_z;
 
 } octant, *p_octant;  //  octant is a cube divided into 8 cubes inside it of equal size
+
+p_octant octant_new(int type, int oct_no);
+
+void 	octant_center_of_mass(p_octant oct);
+void	octant_add_child(p_octant oct, p_octant child);
+
+void 	octant_add_body(p_octant oct, nbody* body);
+void 	octant_del_body(p_octant oct, nbody* body);
+
+
+
 
 #endif
