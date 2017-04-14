ccc180c5975f6450fc172b2eb72aac19247d940e
jsburke <jsburke@bu.edu>
2017-04-14 13:31:59 -0400

    FORCE_PARTIAL to Macro, fix and add interfaces for body to octant

--- cpu/cpu_simple.h
+++ cpu/cpu_simple.h
@@ -4,13 +4,15 @@
 #include <math.h>
 #include "../octree/octree.h"
 
+const data_t GRAV_CONST = 6.674e-11;
+
 //  Functions & Macros for the math that will be needed
 #define BODY_BODY_DISTANCE(r_x, r_y, r_z) sqrt((r_x * r_x) + (r_y * r_y) + (r_z * r_z))
-data_t 	body_body_partial_force(int focus, int other, p_octant oct);
+#define FORCE_PARTIAL(m1, m2, r) GRAV_CONST * ((m1 * m2)/(r * r * r))
 void 	body_body_accum_accel(int focus, int other, p_octant oct);
 
-data_t 	body_oct_distance(int body, p_octant oct);
-data_t 	body_oct_force(int body, p_octant oct);
+data_t 	body_oct_distance(p_octant local, int body, p_octant distal);
+void 	body_oct_accum_accel(p_octant local, int body, p_octant distal);
 //  not doing accumulation of acceleration since we only need three divides and accums
 
 #endif
