e18182f889c96656c9712423b61cbb00bed18e70
jsburke <jsburke@bu.edu>
2017-04-14 11:55:10 -0400

    note on thing to fix

--- cpu/cpu_simple.c
+++ cpu/cpu_simple.c
@@ -14,7 +14,7 @@ data_t body_body_distance(int focus, int other, p_octant oct)
 	return sqrt(x_diff*x_diff + y_diff*y_diff + z_diff*z_diff);
 }
 
-data_t body_body_force(int focus, int other, p_octant oct)
+data_t body_body_force(int focus, int other, p_octant oct)  // not completely right
 {
 	data_t distance = body_body_distance(focus, other, oct);
 	data_t dist_sq	= distance * distance;
@@ -22,4 +22,9 @@ data_t body_body_force(int focus, int other, p_octant oct)
 	//  G * (m1 * m2) / r^2
 
 	return GRAV_CONST * (((oct->mass[focus]) * (oct->mass[other])) / dist_sq);
+}
+
+void   body_body_accum_accel(int focus, int other, p_octant oct)
+{
+	data_t force = body_body_force(focus, other, oct);
 }
