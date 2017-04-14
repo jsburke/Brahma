afddd7699d09e08141df82cf21ca510ec701d0ff
jsburke <jsburke@bu.edu>
2017-04-14 11:16:10 -0400

    distance calculation, include math.h

--- cpu/cpu_simple.c
+++ cpu/cpu_simple.c
@@ -1,1 +1,13 @@
 #include "cpu_simple.h"
+#include <math.h>
+
+data_t body_body_distance(int focus, int other, p_octant oct)
+{
+	// first get the orthogonal diffs
+	int x_diff = (oct->pos_x[focus]) - (oct->pos_x[other]);
+	int y_diff = (oct->pos_y[focus]) - (oct->pos_y[other]);
+	int z_diff = (oct->pos_z[focus]) - (oct->pos_z[other]);
+
+	// square and squareroot
+	return sqrt(x_diff*x_diff + y_diff*y_diff + z_diff*z_diff);
+}
