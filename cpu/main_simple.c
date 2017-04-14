bb7c7c425b6466a8f59deae1ee6f6d34e081771f
jsburke <jsburke@bu.edu>
2017-04-14 17:53:20 -0400

    read filename from command line, extract body count

--- cpu/main_simple.c
+++ cpu/main_simple.c
@@ -1,10 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "cpu_simple.h"
 
 #define REBUILD_FREQ	5		// Rebuild after every X iterations
 #define TIME_STEP 		30  	// in simulation time, in minutes
 #define EXIT_COUNT		200 	// Number of iterations to do before exiting, maybe 0 or -1 for infinite
+#define FILENAME_LEN 	256
+
 
 ////////////////////////////////////////////////////////////////////
+//
+// Inputs: a file name of the form galaxy_###.csv, where the number
+//		   portion may be arbitrarily long
 //
 // Program Outline
 //
@@ -23,3 +31,36 @@
 //			zero out accelerations
 //
 ////////////////////////////////////////////////////////////////////
+
+int main(int argc, char *argv[])
+{
+	//  grab and process the file from command line
+	char*		filename = (char*) malloc(sizeof(char) * FILENAME_LEN);
+	nbody*		bodies;
+	int 		num_bodies = 0;
+
+	if(argc != 2)
+	{
+		printf("\nERROR: Comman line requires file name input!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	filename = argv[1];
+
+	//  calculate of bodies
+	//  NB, the loop here is designed with only galaxy_####.csv as an expected name
+	//  No error checking done for variety of inputs
+	char* p = filename;
+	while(*p)	//  Still more characters to process
+	{
+		if(isdigit(*p))
+		{
+			num_bodies *= 10;
+			num_bodies += strtol(p, &p, 10);
+		}
+		p++;
+	}
+
+
+	return 0;
+}
