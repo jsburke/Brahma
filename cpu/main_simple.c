8f0e5a92cc459a3079634c8c6e94f41d01cd5aca
jsburke <jsburke@bu.edu>
2017-04-14 14:21:12 -0400

    start main for simple octree

---
+++ cpu/main_simple.c
@@ -0,0 +1,5 @@
+#include "cpu_simple.h"
+
+#define REBUILD_FREQ	5	// Rebuild after every X iterations
+#define TIME_STEP 		30  // in simulation time, in minutes
+#define EXIT_COUNT		200 // Number of iterations to do before exiting, maybe 0 or -1 for infinite
