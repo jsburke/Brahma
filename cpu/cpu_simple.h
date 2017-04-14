c1ca76828a5adfea51aa4bca5339cb800031eca5
jsburke <jsburke@bu.edu>
2017-04-14 08:58:25 -0400

    function interfaces in header, include in implementation

---
+++ cpu/cpu_simple.h
@@ -0,0 +1,15 @@
+#ifndef CPU_SIMPLE_H
+#define CPU_SIMPLE_H
+
+#include "../octree/octree.h"
+
+//  Functions for the math that will be needed
+data_t 		body_body_distance(int focus, int other);
+data_t 		body_body_force(int focus, int other);
+data_t 		body_body_accum_accel(int focus, int other);
+
+data_t 		body_oct_distance(int body, p_octant oct);
+data_t 		body_oct_force(int body, p_octant oct);
+//  not doing accumulation of acceleration since we only need three divides and accums
+
+#endif
