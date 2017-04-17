c9c7142d29763d0417a6bd61e46258cc4b5cf96d
jsburke <jsburke@bu.edu>
2017-04-17 10:12:18 -0400

    add failure on data_t poor definition

--- cpu/main_simple.c
+++ cpu/main_simple.c
@@ -3,11 +3,10 @@
 #include <ctype.h>
 #include "cpu_simple.h"
 
-#define REBUILD_FREQ	5		// Rebuild after every X iterations
-#define TIME_STEP 		30  	// in simulation time, in minutes
-#define EXIT_COUNT		200 	// Number of iterations to do before exiting, maybe 0 or -1 for infinite
-#define FILENAME_LEN 	256
-
+#define REBUILD_FREQ			5		// Rebuild after every X iterations
+#define TIME_STEP 				30  	// in simulation time, in minutes
+#define EXIT_COUNT				200 	// Number of iterations to do before exiting, maybe 0 or -1 for infinite
+#define FILENAME_LEN 			256
 
 ////////////////////////////////////////////////////////////////////
 //
@@ -34,6 +33,18 @@
 
 int main(int argc, char *argv[])
 {
+	/////////////////
+	//
+	// data_t check
+	//
+	/////////////////
+
+	if(MAX_POS_POSITION == DATA_T_ERR)
+	{
+		printf("\nERROR: data_t not defined properly!\n");
+		return 0;
+	}
+
 	/////////////////
 	//
 	//  PART 1
