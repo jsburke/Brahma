29bc56860b6804e6d1fa15292c6c9aac697c48b3
jsburke <jsburke@bu.edu>
2017-04-19 14:06:12 -0400

    implement force_accum

--- cpu/cpu_n2n.c
+++ cpu/cpu_n2n.c
@@ -28,3 +28,32 @@ void	force_zero(data_t* x, data_t* y, data_t* z, int len)
 		z[i] = 0;
 	}
 }
+
+void	force_accum(data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* frc_x, data_t* frc_y, data_t* frc_z, int focus, int comp)
+{
+	//  First the distance
+	data_t r_x, r_y, r_z, r;
+
+	r_x = pos_x[focus] - pos_x[comp];
+	r_y = pos_y[focus] - pos_y[comp];
+	r_z = pos_z[focus] - pos_z[comp];
+
+	r = DISTANCE(r_x, r_y, r_z);
+
+	//  then the force for the focus
+
+	data_t F_part;
+
+	F_part = FORCE_PARTIAL(mass[focus], mass[comp], r);
+
+	frc_x[focus]  += F_part * r_x;
+	frc_y[focus]  += F_part * r_y;
+	frc_z[focus]  += F_part * r_z;
+
+	// force for the comparison
+	// we know this by Newton's 3rd law
+
+	frc_x[comp]   += -frc_x[focus];
+	frc_y[comp]   += -frc_y[focus];
+	frc_z[comp]   += -frc_z[focus];
+}
