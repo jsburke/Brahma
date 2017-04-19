34a865c4de7567b91e962d561361d2599e23c492
jsburke <jsburke@bu.edu>
2017-04-19 13:49:43 -0400

    add payload loop function interfaces

--- cpu/cpu_n2n.h
+++ cpu/cpu_n2n.h
@@ -15,6 +15,11 @@ typedef float data_t;
 #define DISPLACE(vel, accel, time) time * (vel + (0.5 * accel * time))
 
 int 	body_count(char* filename);  //const char* ???
+
 void	force_zero(data_t* x, data_t* y, data_t* z, int len);
+void	force_accum(data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* frc_x, data_t* frc_y, data_t* frc_z, int focus, int comp);
+
+void	position_update(data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* vel_x, data_t* vel_y, data_t* vel_z, data_t* frc_x, data_t* frc_y, data_t* frc_z, int len);
+void	velocity_update(data_t* mass, data_t* vel_x, data_t* vel_y, data_t* vel_z, data_t* frc_x, data_t* frc_y, data_t* frc_z, int len);
 
 #endif
