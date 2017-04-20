616c6419b720a0e8c4c2add4e5d9b63e70c128cb
jsburke <jsburke@bu.edu>
2017-04-19 20:45:35 -0400

    include string.h to fix implicit strlen() calls

--- cpu/cpu_n2n.h
+++ cpu/cpu_n2n.h
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef float data_t;
 #define DATA_T_FLOAT 1  //done for conditional compile, change if data_t is change
