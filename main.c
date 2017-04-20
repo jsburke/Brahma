a058916dd4d109c8781d5b744a600214497acba8
sarthakjagetia <sarthakj@bu.edu>
2017-04-19 20:17:28 -0400

    GPU code
    GPU code for n to n computation

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-#include "cpu_simple.h"
+//#include "cpu_simple.h"
 #include <cstdio>
 #include <cstdlib>
 #include <math.h>
 #include <time.h>
+#include "cpu_n2n.h"
 
-#define REBUILD_FREQ			5			// Rebuild after every X iterations
+#define REBUILD_FREQ		5			// Rebuild after every X iterations
 #define TIME_STEP 			30  			// in simulation time, in minutes
 #define EXIT_COUNT			200 			// Number of iterations to do before exiting, -1 for infinite
-#define FILENAME_LEN 			256
+#define FILENAME_LEN 		256
 #define ERROR 				-1 			// Generic Error val for readability
 
 #define SECS				TIME_STEP * 60		// seconds per time step
-#define G    				6.67408e-11;
+#define GRAV_CONST  	  	6.67408e-11;
 #define num_threads			16
-#define num_blocks			16
+#define num_blocks			1
+#define LINE_LEN			512
 
 typedef float data_t;
 
@@ -103,16 +105,8 @@ int main(int argc, char *argv[])
 	}
 	printf("Num bodies: %d\n", num_bodies);
 	allocSize = sizeof(nbody) * num_bodies;
+	
 
-	//get bodies from file
-/*	h_bodies = (nbody*) malloc(allocSize);
-	h_result = (nbody*) malloc(allocSize);
-	CUDA_SAFE_CALL(cudaMalloc((nbody*)&d_bodies, allocSize));
-	CUDA_SAFE_CALL(cudaMalloc((nbody*)&d_result, allocSize));
-
-	//Transfer the bodies to GPU memory
-	CUDA_SAFE_CALL(cudaMemcpy(d_bodies, h_bodies, allocSize, cudaMemcpyHostToDevice));
-*/
 
 	h_mass  = (data_t*) malloc(sizeof(data_t) * num_bodies);
 
@@ -128,6 +122,13 @@ int main(int argc, char *argv[])
 	h_fma_y = (data_t*) malloc(sizeof(data_t) * num_bodies);
 	h_fma_z = (data_t*) malloc(sizeof(data_t) * num_bodies);
 
+	if(!h_mass || !h_pos_x || !h_pos_y || !h_pos_z || !h_vel_x || !h_vel_y || !h_vel_z || !h_fma_x || !h_fma_y || !h_fma_z)
+	{
+		printf("ERROR: Array malloc issue!\n");
+		return 0;
+	}
+
+	fileread_build_arrays(filename, h_mass, h_pos_x, h_pos_y, h_pos_z, h_vel_x, h_vel_y, h_vel_z, h_fma_x, h_fma_y, h_fma_z, num_bodies)
 	//Allocate memory on GPU
 	CUDA_SAFE_CALL(cudaMalloc((data_t*)&d_mass, allocSize));
 	
@@ -157,14 +158,6 @@ int main(int argc, char *argv[])
 	CUDA_SAFE_CALL(cudaMemcpy(d_fma_x, h_fma_x, allocSize, cudaMemcpyHostToDevice));
 	CUDA_SAFE_CALL(cudaMemcpy(d_fma_y, h_fma_y, allocSize, cudaMemcpyHostToDevice));
 	CUDA_SAFE_CALL(cudaMemcpy(d_fma_z, h_fma_z, allocSize, cudaMemcpyHostToDevice));
-
-
-	if(!h_mass || !h_pos_x || !h_pos_y || !h_pos_z || !h_vel_x || !h_vel_y || !h_vel_z || !h_fma_x || !h_fma_y || !h_fma_z)
-	{
-		printf("ERROR: Array malloc issue!\n");
-		return 0;
-	}
-
 	
 	if( KILL == nbody_enum(bodies, filename)) return 0;  // exit on failure
 	free(filename);  //  file will no longer be accessed
@@ -176,16 +169,15 @@ int main(int argc, char *argv[])
 	
 	for(i = 0; i < EXIT_COUNT; i++)
 	{
-		force_zero(fma_x, fma_y, fma_z, num_bodies);
+		kernel_force_zero<<<dimGrid, dimBlock>>>(fma_x, fma_y, fma_z, num_bodies);
 
-		for(j = 0; j < num_bodies; j++)
-		{
-			for(k = j + 1; k < num_bodies; k++)
-				force_accum(mass, pos_x, pos_y, pos_z, fma_x, fma_y, fma_z, j, k);
-		}
+		//for(j = 0; j < num_bodies; j++){
+			//for(k = j + 1; k < num_bodies; k++)
+		kernel_force_accum<<<dimGrid, dimBlock>>>(mass, pos_x, pos_y, pos_z, fma_x, fma_y, fma_z, j, k);
+		//}
 
-		position_update(mass, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, fma_x, fma_y, fma_z, num_bodies, TIME_STEP);
-		velocity_update(mass, vel_x, vel_y, vel_z, fma_x, fma_y, fma_z, num_bodies, TIME_STEP);
+		kernel_position_update<<<dimGrid, dimBlock>>>(mass, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, fma_x, fma_y, fma_z, num_bodies, TIME_STEP);
+		kernel_velocity_update<<<dimGrid, dimBlock>>>(mass, vel_x, vel_y, vel_z, fma_x, fma_y, fma_z, num_bodies, TIME_STEP);
 		//  if we get graphics in, update screen here
 	}
 
@@ -199,36 +191,144 @@ int main(int argc, char *argv[])
 	CUDA_SAFE_CALL(cudaFree(d_bodies));
 	CUDA_SAFE_CALL(cudaFree(d_result));
 		   
-	free(h_bodies);
-	free(h_result);
+	//free(h_bodies);
+	//free(h_result);
 
 	return 0;
 }
 
 
-__global__ void kernel_forceCalc(data_t * d_mass, data_t * d_pos_x, data_t * d_pos_y, data_t * d_pos_z, data_t * d_vel_x, data_t * d_vel_y, data_t * d_vel_z, data_t * d_fma_x, data_t * d_fma_y, data_t * d_fma_z) {
+__global__ void kernel_force_zero(data_t* x, data_t* y, data_t* z, int num_bodies)
+{
+	int i;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
 
-	long int i;
-	float 	F, r_x, r_y, r_z, r;
-	unsigned int index_x = blockIdx.x * blockDim.x + threadIdx.x;
-   	unsigned int index_y = blockIdx.y * blockDim.y + threadIdx.y;	
-    	unsigned int index = index_y * num_bodies + index_x;
+	for(i = 0; i < num_bodies; i++)
+	{
+		int idx = index * num_bodies + i;
+		x[idx] = 0;
+		y[idx] = 0;
+		z[idx] = 0;
+	}
+}
 
-	for (int i = 0; i<num_bodies; i++){
-        	for (int j = 0; j<num_bodies; j++){
-            		r_x = (abs(bodies[j].xPosition - bodies[i].xPosition));
-		        r_y = (abs(bodies[j].yPosition - bodies[i].yPosition));
-            		r_z = (abs(bodies[j].zPosition - bodies[i].zPosition));
-            		r = sqrt(r_x*r_x + r_y*r_y + r_z*r_z);
-            		F = (G*bodies[i].mass*bodies[j].mass)/(r*r);
-            		if(i != j){
-                	printf ("Force between body %d and %d is:%fN\n",i,j,F);
-            }
-        }
-	for (i=0;i<N;i++)
-		sum += x[index_y*N+i]*y[index_x+i*N]; 
+__global__ void kernel_force_accum(data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* fma_x, data_t* fma_y, data_t* fma_z, int num_bodies)
+{
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+	int i;
+
+	//  First the distance
+	data_t* r_x, r_y, r_z, r;
+	data_t* F_part;
+	for(i = 0; i < num_bodies; i++){
+		int idx = index * num_bodies + i;
+		if(i != index){
+			r_x[idx] = pos_x[idx] - pos_x[i];
+			r_y[idx] = pos_y[idx] - pos_y[i];
+			r_z[idx] = pos_z[idx] - pos_z[i];
 	
-	result[index] = sum; 
+			r[idx] = sqrt((r_x[idx] * r_x[idx]) + (r_y[idx] * r_y[idx]) + (r_z * r_z[idx]));
+
+			//  then the force for the focus
+			F_part[idx] = GRAV_CONST * (mass[idx] * mass[i])/(r[idx] * r[idx] *r[idx]);
+
+			fma_x[idx]  += F_part[idx] * r_x[idx];
+			fma_y[idx]  += F_part[idx] * r_y[idx];
+			fma_z[idx]  += F_part[idx] * r_z[idx];
+
+			// force for the comparison
+			// we know this by Newton's 3rd law
+	
+			fma_x[i]   += -fma_x[idx];
+			fma_y[i]   += -fma_y[idx];
+			fma_z[i]   += -fma_z[idx];
+			}
+	}
+}
+
+__global__ void kernel_position_update(data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* vel_x, data_t* vel_y, data_t* vel_z, data_t* fma_x, data_t* fma_y, data_t* fma_z, int num_bodies, int time)
+{
+	//  NB, when this is invoked, fma arrays will have forces built up in force_accum()
+	int i;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for(i = 0; i < len; i++)
+	{
+		int idx = index * num_bodies + i;
+
+		// convert forces to acceleration, saves a multiply later
+		fma_x[idx] /= mass[idx];
+		fma_y[idx] /= mass[idx];
+		fma_z[idx] /= mass[idx];
+
+		pos_x[idx] += time * (vel_x[idx] + (0.5 * fma_x[idx] * time)); 
+		pos_y[idx] += time * (vel_y[idx] + (0.5 * fma_y[idx] * time)); 
+		pos_z[idx] += time * (vel_z[idx] + (0.5 * fma_z[idx] * time)); 
+	}
+}
+
+__global__ void kernel_velocity_update(data_t* mass, data_t* vel_x, data_t* vel_y, data_t* vel_z, data_t* fma_x, data_t* fma_y, data_t* fma_z, int num_bodies, int time)
+{
+	// NB, when this is invoked, fma arrays should be accelerations set in position_update()
+	int i;
+	int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+	for(i = 0; i < num_bodies; i++)
+	{
+		int idx = index * num_bodies + i;
+		vel_x[idx] += fma_x[idx] * time;
+		vel_y[idx] += fma_y[idx] * time;
+		vel_z[idx] += fma_z[idx] * time;
+	}
+}
+
+int fileread_build_arrays(char* filename, data_t* mass, data_t* pos_x, data_t* pos_y, data_t* pos_z, data_t* vel_x, data_t* vel_y, data_t* vel_z, int len)
+{
+	// returns true -- false
+	FILE *fp = fopen(filename, "r");
+
+	if(fp == NULL) return 0;
+
+	int i = 0;
+	char *buf = (char*) malloc(LINE_LEN);
+	int buf_len = 0;
+
+	while((i < len) && (fgets(buf, LINE_LEN - 1, fp) != NULL))
+	{
+		buf_len = strlen(buf);
+
+		if((buf_len > 0) && (buf[buf_len - 1] == '\n'))
+			buf[buf_len - 1] = '\0'; 
+
+		tmp = strtok(buf, ",");
+        //nbodies[i].category = tmp;
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_mass = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_pos_x = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_pos_y = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_pos_z = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_vel_x = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_vel_y = atof(tmp);
+
+        tmp = strtok(NULL, ",");
+        nbodies[i].h_vel_z = atof(tmp);
+
+		i++;
+	}
+	free(buf);
+	fclose(fp);
+	return 1;
 }
 
 
