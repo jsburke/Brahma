8fa22ae2c00b2edb28beb0f1180197e800d6f591
jsburke <jsburke@bu.edu>
2017-04-28 08:28:31 -0400

    generalize file gen and code execution

--- util/sim_run.pl
+++ util/sim_run.pl
@@ -28,6 +28,9 @@ if (-e $octomp) {
   unlink($octomp);
 }
 
+$bodyGen = "../util/bodyGen.py";
+@objectCounts = (100, 500, 1000, 2000, 4000, 10000, 25000);
+
 print "building $n2n, $oct, and $octomp\n";
 system("make");
 if (!(-x $n2n)) {
@@ -43,115 +46,38 @@ if (!(-x $octomp)) {
 }
 
 print "\nProduce CSV files for execution\n";
-# 100
-system("../util/bodyGen.py --total 100");
 
-# 500
-system("../util/bodyGen.py --total 500");
-
-# 1000
-system("../util/bodyGen.py --total 1000");
-
-# 2000
-system("../util/bodyGen.py --total 2000");
-
-# 4000
-system("../util/bodyGen.py --total 4000");
-
-# 10000
-system("../util/bodyGen.py --total 10000");
-
-# 25000
-system("../util/bodyGen.py --total 25000");
+foreach $count(@objectCounts)
+{
+	system("$bodyGen --total $count");
+}
 
 # for spacing
 print "  BEGIN EXECUTION \n\n";
 
 # run a series of tests
 # harvest data
-print "N2N 100:\n";
-system("$n2n galaxy_100.csv");
-print "\n";
 
-print "N2N 500:\n";
-system("$n2n galaxy_500.csv");
-print "\n";
+foreach $count(@objectCounts)
+{
+	print "N2N $count:\n";
+	system("$n2n galaxy_$count.csv");
+	print "\n";
+}
 
-print "N2N 1000:\n";
-system("$n2n galaxy_1000.csv");
-print "\n";
+foreach $count(@objectCounts)
+{
+	print "OCTREE $count:\n";
+	system("$oct galaxy_$count.csv");
+	print "\n";
+}
 
-print "N2N 2000:\n";
-system("$n2n galaxy_2000.csv");
-print "\n";
-
-print "N2N 4000:\n";
-system("$n2n galaxy_4000.csv");
-print "\n";
-
-print "N2N 10000:\n";
-system("$n2n galaxy_10000.csv");
-print "\n";
-
-print "N2N 25000\n";
-system("$n2n galaxy_25000.csv");
-print "\n";
-
-print "OCTREE 100:\n";
-system("$oct galaxy_100.csv");
-print "\n";
-
-print "OCTREE 500:\n";
-system("$oct galaxy_500.csv");
-print "\n";
-
-print "OCTREE 1000:\n";
-system("$oct galaxy_1000.csv");
-print "\n";
-
-print "OCTREE 2000:\n";
-system("$oct galaxy_2000.csv");
-print "\n";
-
-print "OCTREE 4000:\n";
-system("$oct galaxy_4000.csv");
-print "\n";
-
-print "OCTREE 10000:\n";
-system("$oct galaxy_10000.csv");
-print "\n";
-
-print "OCTREE 25000\n";
-system("$oct galaxy_25000.csv");
-print "\n";
-
-print "OCTREE_OMP 100:\n";
-system("$octomp galaxy_100.csv");
-print "\n";
-
-print "OCTREE_OMP 500:\n";
-system("$octomp galaxy_500.csv");
-print "\n";
-
-print "OCTREE_OMP 1000:\n";
-system("$octomp galaxy_1000.csv");
-print "\n";
-
-print "OCTREE_OMP 2000:\n";
-system("$octomp galaxy_2000.csv");
-print "\n";
-
-print "OCTREE_OMP 4000:\n";
-system("$octomp galaxy_4000.csv");
-print "\n";
-
-print "OCTREE_OMP 10000:\n";
-system("$octomp galaxy_10000.csv");
-print "\n";
-
-print "OCTREE_OMP 25000\n";
-system("$octomp galaxy_25000.csv");
-print "\n";
+foreach $count(@objectCounts)
+{
+	print "OCTREE_OMP $count:\n";
+	system("$octomp galaxy_$count.csv");
+	print "\n";
+}
 
 print "clean up\n";
 system("rm *.csv");
