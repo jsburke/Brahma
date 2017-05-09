641daa2b6d43e01665a454fe863d02165b4bc730
jsburke <jsburke@bu.edu>
2017-05-09 16:33:14 -0400

    matlab to generate plots from output

---
+++ util/results_plot.m
@@ -0,0 +1,33 @@
+function results_plot( file )
+
+%read data
+data = csvread(file, 0, 1);
+x    = data(1, 1:end-1);
+res  = data(2:end, 1:end-1);
+[opts extra] = size(res);
+
+%get method names
+fid  = fopen(file);
+i    = 0;
+methods = {};
+
+while ~feof(fid)
+   i = i + 1;
+   method =  strsplit(fgetl(fid),',');
+   if i > 1
+        methods{i-1} = method{1};
+   end
+end
+
+% set up plot
+figure;
+colors  = ['b','r','m','k','g'];   % colors for plots
+
+for i = 1:opts
+    plot(x, res(i,:), 'Color', colors(i));
+    hold on;
+end
+hold off;
+legend(methods);
+end
+
